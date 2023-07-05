cfcd66bc255899cd6834a8e98d4eece6d86c184f
Ehsan-ML79 <125207813+Ehsan-ML79@users.noreply.github.com>
2023-07-05 23:04:09 +0430

    force to send message

--- Lightbtn.js
+++ Lightbtn.js
@@ -4,13 +4,13 @@ var subscribeBtn= document.getElementById('subscribe-btn');
 subscribeBtn.addEventListener('click', function(event){
     alert('tnx for subscribing !\nyou will noticed newest our product!!');
     event.preventDefault();
-})
+});
 
 //Contact pgae
-var contactBtn = document.getElementById('send-btn');
-//if (contactBtn) {
+document.addEventListener('DOMContentLoaded', function() {
+  var contactBtn = document.getElementById('send-btn');
   contactBtn.addEventListener('click', function(event) {
     alert('پیام شما ارسال شد!\nپاسخ شما در اسرع وقت داده خواهد شد');
     event.preventDefault();
-  //});
-})
+  });
+});
