33af6f2c2fb46bfb355af67eb7499d113ab8198a
Ehsan-ML79 <125207813+Ehsan-ML79@users.noreply.github.com>
2023-07-05 11:47:42 +0430

    rearranging & recommenting

--- item2cart2.1.js
+++ item2cart2.1.js
@@ -1,45 +1,75 @@
-// Retrieve the cart from localStorage
+//initializing cart's data
 var cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 // Get the cart list element
 var cartList = document.getElementById('cart-list');
 
-// Function to calculate and display the total price
-function displayTotalPrice() {
-  // Calculate the total price
-  var totalPrice = 0;
+// Get all "افزودن به سبد خرید" buttons
+const addToCartButtons = document.querySelectorAll('.product .btn');
+addToCartButtons.forEach(button => {
+  button.addEventListener('click', addToCart);
+});
 
-  // Iterate over each item in the cart
+// Handle adding item2cart
+function addToCart(event) {
+  event.preventDefault();
+
+  // Product details
+  const product = event.target.parentNode;
+  const productName = product.querySelector('h3').textContent;
+  const productPrice = product.querySelector('p').textContent;
+
+  // Create an object with the product details
+  const item = {
+    name: productName,
+    price: productPrice,
+    quantity: 1
+  };
+
+  // Add item2cart
+  cart.push(item);
+
+  // Update the cart
+  localStorage.setItem('cart', JSON.stringify(cart));
+
+  alert('محصول با موفقیت به سبد خرید اضافه شد!');
+
+  // Re-display the cart items
+  displayCartItems();
+  
+}
+// Function 4 the total price
+function displayTotalPrice() {
+
+  var totalPrice = 0;
+  
   cart.forEach(function(item) {
-    // Get the quantity of the item
+    // Get the quantity
     var quantity = parseInt(item.quantity);
 
-    // Check if the quantity is a valid number
+    // Check validity of quantity
     if (!isNaN(quantity) && quantity > 0) {
-      // Calculate the item's total price
+      
       var itemTotalPrice = parseInt(item.price) * quantity;
-
-      // Add the item's total price to the overall total price
+      
       totalPrice += itemTotalPrice;
     }
   });
 
-  // Update the total price element
+  // Update total price
   var totalPriceElement = document.getElementById('total-price-value');
   totalPriceElement.textContent = totalPrice;
 }
 
-// Function to display the items in the cart
+// Display items' cart
 function displayCartItems() {
-  // Clear the existing list
+  // Clear list
   cartList.innerHTML = '';
 
-  // Calculate the total price
   var totalPrice = 0;
 
-  // Iterate over each item in the cart
   cart.forEach(function(item) {
-    // If the item doesn't have a valid quantity, set it to 1
+    // checking valid quantity, set it to 1
     if (!item.quantity || isNaN(item.quantity) || item.quantity <= 0) {
       item.quantity = 1;
     }
@@ -47,14 +77,14 @@ function displayCartItems() {
     // Check if the item already exists in the cart
     var existingItem = cartList.querySelector(`li[data-name="${item.name}"]`);
 
+    //Duplicating issue ...
     if (existingItem) {
-      // If the item exists, increment its quantity
-      var quantityElement = existingItem.querySelector('.quantity');
-      var quantity = parseInt(quantityElement.textContent);
-      quantityElement.textContent = quantity + 1;
-      item.quantity = quantity + 1;
-    } else {
-      // If the item doesn't exist, create a new list item
+      
+      alert("error duplicating!");
+    }
+
+    else {
+      // create a new list item
       var listItem = document.createElement('li');
       listItem.dataset.name = item.name;
 
@@ -71,14 +101,14 @@ function displayCartItems() {
       const decreaseButton = document.createElement('button');
       decreaseButton.textContent = '-';
       decreaseButton.addEventListener('click', function() {
-        // Decrease the quantity of the item
+        
         var quantityElement = this.nextElementSibling;
         var quantity = parseInt(quantityElement.textContent);
         if (quantity > 1) {
           quantityElement.textContent = quantity - 1;
           item.quantity = quantity - 1;
           localStorage.setItem('cart', JSON.stringify(cart));
-          displayTotalPrice(); // Update the total price
+          displayTotalPrice(); // Update
         }
       });
 
@@ -89,36 +119,35 @@ function displayCartItems() {
       const increaseButton = document.createElement('button');
       increaseButton.textContent = '+';
       increaseButton.addEventListener('click', function() {
-        // Increase the quantity of the item
+        
         var quantityElement = this.previousElementSibling;
         var quantity = parseInt(quantityElement.textContent);
         quantityElement.textContent = quantity + 1;
         item.quantity = quantity + 1;
         localStorage.setItem('cart', JSON.stringify(cart));
-        displayTotalPrice(); // Update the total price
+        displayTotalPrice(); // Update
       });
 
-      // Create remove button
       var removeButton = document.createElement('button');
       removeButton.textContent = 'حذف';
       removeButton.addEventListener('click', function() {
-        // Remove the item from the cart
+        // Removing process...
         var listItem = this.closest('li');
         var itemName = listItem.dataset.name;
 
         listItem.remove();
 
+        // Updating new cart with omitting the removed item 
         cart = cart.filter(function(item) {
           return item.name !== itemName;
         });
 
         localStorage.setItem('cart', JSON.stringify(cart));
 
-        // Update the total price
         displayTotalPrice();
       });
 
-      // Append elements to the list item
+      // Append Qbuttons to the list Wrapper
       quantityWrapper.appendChild(decreaseButton);
       quantityWrapper.appendChild(quantityElement);
       quantityWrapper.appendChild(increaseButton);
@@ -137,59 +166,22 @@ function displayCartItems() {
     totalPrice += itemTotalPrice;
   });
 
-  // Update the total price element
+  // Update the total price
   var totalPriceElement = document.getElementById('total-price-value');
   totalPriceElement.textContent = totalPrice;
 }
 
-// Display the cart items initially
+// Display initially
 displayCartItems();
 
-// Purchase button
 var purchaseButton = document.getElementById('purchase-button');
 purchaseButton.addEventListener('click', function() {
   alert('با تشکر از خرید شما!');
   // Reset the cart and update localStorage
   cart = [];
   localStorage.setItem('cart', JSON.stringify(cart));
-  // Re-display the cart items
   displayCartItems();
 });
 
-// Get all the "افزودن به سبد خرید" buttons
-const addToCartButtons = document.querySelectorAll('.product .btn');
 
-// Add event listener to each button
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', addToCart);
-});
 
-// Function to handle adding items to the cart
-function addToCart(event) {
-  event.preventDefault();
-
-  // Get the product details
-  const product = event.target.parentNode;
-  const productName = product.querySelector('h3').textContent;
-  const productPrice = product.querySelector('p').textContent;
-
-  // Create an object with the product details
-  const item = {
-    name: productName,
-    price: productPrice,
-    quantity: 1 // Set the initial quantity to 1
-  };
-
-  // Add the item to the cart
-  cart.push(item);
-
-  // Update the cart in the localStorage
-  localStorage.setItem('cart', JSON.stringify(cart));
-
-  // Show a success message to the user
-  alert('محصول با موفقیت به سبد خرید اضافه شد!');
-
-  // Re-display the cart items
-  displayCartItems();
-  
-}
--- js_add2cart2.js
+++ js_add2cart2.js
@@ -1,59 +1,58 @@
-// JavaScript code for adding items to the cart
+// JS for adding2cart
 function addToCartFunction(item) {
-    // Check if the cart exists in the localStorage
+
+    // Check whether CART exists or not
     var cart = localStorage.getItem('cart');
     if (cart === null) {
-      // If the cart doesn't exist, create an empty array
+      // Create an empty array
       cart = [];
-    } else {
-      // If the cart exists, parse the JSON string to an array
+    } 
+    else {
+      // parse the JSON string to an array
       cart = JSON.parse(cart);
     }
-  
-    // Check if the item already exists in the cart
+
+    // Get all "افزودن به سبد خرید" buttons
+  const addToCartButtons = document.querySelectorAll('.product .btn');
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', addToCart);
+  });
+    
+    function addToCart(event) {
+      event.preventDefault();
+    
+      // Get the product details
+      const product = event.target.parentNode;
+      const productName = product.querySelector('h3').textContent;
+      const productPrice = product.querySelector('p').textContent;
+    
+      // Create an object with the product details
+      const item = {
+        name: productName,
+        price: productPrice
+      };
+      
+    // Check whether ITEM already exists or not
     var existingItem = cart.find(function(cartItem) {
       return cartItem.name === item.name;
     });
   
     if (existingItem) {
-      // If the item already exists, display a message to the user
+      
       alert('The item has already been added!');
-    } else {
-      // Add the item to the cart
+    } 
+    else {
       cart.push(item);
   
-      // Update the cart in the localStorage
+      // Updating
       localStorage.setItem('cart', JSON.stringify(cart));
   
-      // Show a success message to the user
       alert('محصول با موفقیت به سبد خرید اضافه شد!');
     }
   }
   
-  // Get all the "افزودن به سبد خرید" buttons
-  const addToCartButtons = document.querySelectorAll('.product .btn');
   
-  // Add event listener to each button
-  addToCartButtons.forEach(button => {
-    button.addEventListener('click', addToCart);
-  });
-  
-  // Function to handle adding items to the cart
-  function addToCart(event) {
-    event.preventDefault();
-  
-    // Get the product details
-    const product = event.target.parentNode;
-    const productName = product.querySelector('h3').textContent;
-    const productPrice = product.querySelector('p').textContent;
-  
-    // Create an object with the product details
-    const item = {
-      name: productName,
-      price: productPrice
-    };
-  
-    // Add the item to the cart
+    // Add2the cart
     addToCartFunction(item);
   }
   
